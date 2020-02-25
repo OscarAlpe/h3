a4f14f8b653d0404939ea0fd57acb0943803a74d
Oscar Megía López <pfgytk@gmail.com>
2020-02-25 12:04:42 +0100

    Ejercicio 3: En la carpeta vistas dejar solamente la vista error.php
    Personalizar la vista.
    Para que salga el error he tenido que borrar todas las acciones del site Controller.

--- views/site/error.php
+++ views/site/error.php
@@ -18,10 +18,10 @@ $this->title = $name;
     </div>
 
     <p>
-        The above error occurred while the Web server was processing your request.
+        Upss.....
     </p>
     <p>
-        Please contact us if you think this is a server error. Thank you.
+        Hay un problema
     </p>
 
 </div>
