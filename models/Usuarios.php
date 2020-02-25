61c972092ee8ab2f9d30f2829d2c9e4e7500c25d
Oscar Megía López <pfgytk@gmail.com>
2020-02-25 13:25:43 +0100

    Ejercicio 7: Vamos a gestionar los formularios utilizando modelos (todavía no tenemos nada de bases de datos).
    Podemos utilizar modelos sin necesidad de bases de datos.
    Vamos a crear un formulario utilizando un modelo.
    Creamos primero el modelo y le llamamos Usuarios.

---
+++ models/Usuarios.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class Usuarios extends Model {
+    public $nombre;
+    public $apellidos;
+    public $edad;
+    public $email;
+    
+    public function rules() {
+        return [
+          [['nombre','edad','email'], 'required']  ,
+          ['email','email']
+        ];
+    }
+    
+    public function attibuteLabels() {
+        return [
+            'nombre' => 'Nombre de usuario',
+        ];
+    }
+}
